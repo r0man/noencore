4da2e2d4a3eac252192b48ed7016472c9db207f3
Roman Scherer <roman.scherer@burningswell.com>
2013-12-16 20:05:09 +0100

    lein-release plugin: bumped version from 0.1.10 to 0.1.11-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject noencore "0.1.10"
+(defproject noencore "0.1.11-SNAPSHOT"
   :description "Clojure and ClojureScript fns not in core."
   :url "http://github.com/r0man/noencore"
   :author "Roman Scherer"
