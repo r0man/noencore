663a7c7b29fa0a5b743c516372b44564c7e6383b
Roman Scherer <roman.scherer@burningswell.com>
2014-01-19 13:13:29 +0100

    Updated dependencies.

--- project.clj
+++ project.clj
@@ -6,31 +6,31 @@
   :lein-release {:deploy-via :clojars}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[commons-codec/commons-codec "1.8"]
-                 [org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2120"]]
-  :profiles {:dev {:dependencies [[com.keminglabs/cljx "0.3.1"]]
-                   :plugins [[com.cemerick/austin "0.1.3"]]
-                   :repl-options {:nrepl-middleware [cljx.repl-middleware/wrap-cljx]}}}
-  :plugins [[com.cemerick/clojurescript.test "0.2.1"]
-            [com.keminglabs/cljx "0.3.1"]
-            [lein-cljsbuild "1.0.1"]]
-  :hooks [cljx.hooks leiningen.cljsbuild]
-  :cljx {:builds [{:source-paths ["src"]
-                   :output-path "target/classes"
-                   :rules :clj}
-                  {:source-paths ["src"]
-                   :output-path "target/classes"
-                   :rules :cljs}
-                  {:source-paths ["test"]
-                   :output-path "target/test-classes"
-                   :rules :clj}
-                  {:source-paths ["test"]
-                   :output-path "target/test-classes"
-                   :rules :cljs}]}
-  :cljsbuild {:test-commands {"phantom" ["phantomjs" :runner "target/testable.js"]}
-              :builds [{:source-paths ["target/classes" "target/test-classes"]
-                        :compiler {:output-to "target/testable.js"
-                                   :optimizations :advanced
-                                   :pretty-print true}}]}
-  :test-paths ["target/test-classes"])
+  :dependencies [[commons-codec/commons-codec "1.9"]]
+  :cljsbuild {:builds []}
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]
+                                  [org.clojure/clojurescript "0.0-2138"]]
+                   :repl-options {:nrepl-middleware [cljx.repl-middleware/wrap-cljx]}
+                   :plugins [[com.keminglabs/cljx "0.3.2"] ;; Must be before Austin: https://github.com/cemerick/austin/issues/37
+                             [com.cemerick/clojurescript.test "0.2.1"]
+                             [com.cemerick/austin "0.1.3"]
+                             [lein-cljsbuild "1.0.1"]]
+                   :hooks [cljx.hooks leiningen.cljsbuild]
+                   :cljx {:builds [{:source-paths ["src"]
+                                    :output-path "target/classes"
+                                    :rules :clj}
+                                   {:source-paths ["src"]
+                                    :output-path "target/classes"
+                                    :rules :cljs}
+                                   {:source-paths ["test"]
+                                    :output-path "target/test-classes"
+                                    :rules :clj}
+                                   {:source-paths ["test"]
+                                    :output-path "target/test-classes"
+                                    :rules :cljs}]}
+                   :cljsbuild {:test-commands {"phantom" ["phantomjs" :runner "target/testable.js"]}
+                               :builds [{:source-paths ["target/classes" "target/test-classes"]
+                                         :compiler {:output-to "target/testable.js"
+                                                    :optimizations :advanced
+                                                    :pretty-print true}}]}
+                   :test-paths ["target/test-classes"]}})
