c44a21016e1907f199c1d7313aa738b17fb40f3a
Roman Scherer <roman.scherer@burningswell.com>
2014-01-24 21:35:52 +0100

    Run tests on node as well.

--- project.clj
+++ project.clj
@@ -12,7 +12,7 @@
                                   [org.clojure/clojurescript "0.0-2138"]]
                    :repl-options {:nrepl-middleware [cljx.repl-middleware/wrap-cljx]}
                    :plugins [[com.keminglabs/cljx "0.3.2"] ;; Must be before Austin: https://github.com/cemerick/austin/issues/37
-                             [com.cemerick/clojurescript.test "0.2.1"]
+                             [com.cemerick/clojurescript.test "0.2.2-SNAPSHOT"]
                              [com.cemerick/austin "0.1.3"]
                              [lein-cljsbuild "1.0.1"]]
                    :hooks [cljx.hooks leiningen.cljsbuild]
@@ -28,7 +28,8 @@
                                    {:source-paths ["test"]
                                     :output-path "target/test-classes"
                                     :rules :cljs}]}
-                   :cljsbuild {:test-commands {"phantom" ["phantomjs" :runner "target/testable.js"]}
+                   :cljsbuild {:test-commands {"node" ["node" :node-runner "target/testable.js"]
+                                               "phantom" ["phantomjs" :runner "target/testable.js"]}
                                :builds [{:source-paths ["target/classes" "target/test-classes"]
                                          :compiler {:output-to "target/testable.js"
                                                     :optimizations :advanced
