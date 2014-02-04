03bc2dea473459321f7594c4931a1801bac84f7d
Roman Scherer <roman.scherer@burningswell.com>
2014-02-04 20:13:52 +0100

    Updated dependencies.

--- project.clj
+++ project.clj
@@ -9,12 +9,12 @@
   :dependencies [[commons-codec/commons-codec "1.9"]]
   :cljsbuild {:builds []}
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]
-                                  [org.clojure/clojurescript "0.0-2138"]]
+                                  [org.clojure/clojurescript "0.0-2156"]]
                    :repl-options {:nrepl-middleware [cljx.repl-middleware/wrap-cljx]}
                    :plugins [[com.keminglabs/cljx "0.3.2"] ;; Must be before Austin: https://github.com/cemerick/austin/issues/37
-                             [com.cemerick/clojurescript.test "0.2.2-SNAPSHOT"]
+                             [com.cemerick/clojurescript.test "0.2.2"]
                              [com.cemerick/austin "0.1.3"]
-                             [lein-cljsbuild "1.0.1"]]
+                             [lein-cljsbuild "1.0.2"]]
                    :hooks [cljx.hooks leiningen.cljsbuild]
                    :cljx {:builds [{:source-paths ["src"]
                                     :output-path "target/classes"
