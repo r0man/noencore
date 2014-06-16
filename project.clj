bcf5995e359dca3360b571999a7cc305573e66d9
Roman Scherer <roman.scherer@burningswell.com>
2014-06-16 10:04:19 +0200

    Update clojurescript

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
   :dependencies [[commons-codec/commons-codec "1.9"]]
   :cljsbuild {:builds []}
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]
-                                  [org.clojure/clojurescript "0.0-2202"]]
+                                  [org.clojure/clojurescript "0.0-2234"]]
                    :repl-options {:nrepl-middleware [cljx.repl-middleware/wrap-cljx]}
                    :plugins [[com.keminglabs/cljx "0.3.2"] ;; Must be before Austin: https://github.com/cemerick/austin/issues/37
                              [com.cemerick/clojurescript.test "0.3.0"]
