313fa129ffd4951287d832112652c2e3326e8d76
Roman Scherer <roman@burningswell.com>
2016-05-14 21:55:16 +0200

    Version 0.3.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject noencore "0.3.1"
+(defproject noencore "0.3.2-SNAPSHOT"
   :description "Clojure and ClojureScript fns not in core."
   :url "http://github.com/r0man/noencore"
   :author "r0man"
