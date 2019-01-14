7b89a8d1cefcc3a1476cd1450e874509448a1f0f
r0man <roman@burningswell.com>
2019-01-14 12:35:03 +0100

    Version 0.3.6-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject noencore "0.3.5"
+(defproject noencore "0.3.6-SNAPSHOT"
   :description "Clojure and ClojureScript fns not in core."
   :url "http://github.com/r0man/noencore"
   :author "r0man"
