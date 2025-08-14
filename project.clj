76c42303b8311258c1d25998f8b7354a4cbb74e5
Roman Scherer <roman@burningswell.com>
2025-08-14 12:11:46 +0200

    Update dependencies

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :min-lein-version "2.0.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.11.3"]]
+  :dependencies [[org.clojure/clojure "1.12.1"]]
   :aliases {"ci" ["do" ["test"] ["doo" "node" "test" "once"] ["lint"]]
             "lint" ["do"  ["eastwood"]]}
   :cljsbuild {:builds [{:id "test"
@@ -16,8 +16,8 @@
                                    :pretty-print true}
                         :source-paths ["src" "test"]}]}
   :deploy-repositories [["releases" :clojars]]
-  :profiles {:dev {:dependencies [[cider/piggieback "0.5.3"]
-                                  [org.clojure/clojurescript "1.11.132"]]
+  :profiles {:dev {:dependencies [[cider/piggieback "0.6.0"]
+                                  [org.clojure/clojurescript "1.12.42"]]
                    :plugins [[jonase/eastwood "1.2.3"]
                              [lein-cljsbuild "1.1.8"]
                              [lein-difftest "2.0.0"]
